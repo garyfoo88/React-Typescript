31f9768cfbe4a53a1179e8d6ba7a81a9c713fe24
garyfoo88 <reid9000200020002000@gmail.com>
2021-06-24 23:17:28 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState<number | string>(5);
+
+  const changeNumber = () => {
+    setNumber("10");
+  };
   return (
     <div className="App">
       <header className="App-header">
